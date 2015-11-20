3ac23e9e44aefd64c1fb14264533c34541a7a1f9
oj-boss <eoo5498@rit.edu>
2015-11-20 14:47:08 -0500

    did some refactoring

--- public/app/controllers/home.js
+++ public/app/controllers/home.js
@@ -6,59 +6,60 @@ angular.module('Home', ['Post', 'DateUtils', 'ngResource', 'ConstFactory'])
         , 'constants'
         , function(top50, $scope, dateUtils, $resource, constants){
 
-        var self = this;
-        var mediaUrl = constants.api + "/media/";
-        self.top50 = top50;
+            var self = this;
+            var mediaUrl = constants.api + "/media/";
+            self.top50 = top50;
 
-        function updateMe(me){
-            var top50 = $resource(constants.api + "/posts/pollTop50");
-            top50.get({}).$promise.then(function(data){
-                if(data.success) {
-                    self.top50 = data;
-                }
-                updateMe(me);
+            function updateMe(me){
+                var top50 = $resource(constants.api + "/posts/pollTop50");
+                top50.get({}).$promise.then(function(data){
+                    if(data.success) {
+                        self.top50 = data;
+                    }
+                    updateMe(me);
+                });
+            }
+
+            updateMe(self.top50);
+
+            var loaded = false;
+            $scope.$on('LastElementReached', function(){
+                $scope.$evalAsync(function() {
+                    setTimeout(function () {
+                        loadCards();
+                    }, 10);
+
+                    if(!loaded) {
+                        setTimeout(function () {
+                            loadCards();
+                        }, 50);
+                        setTimeout(function () {
+                            loadCards();
+                        }, 100);
+
+                        setTimeout(function () {
+                            loadCards();
+                        }, 200);
+                        setTimeout(function () {
+                            loadCards();
+                        }, 1000);
+                        setTimeout(function () {
+                            loadCards();
+                        }, 5000);
+
+                        loaded = true;
+                    }
+                });
             });
+
+            self.getMediaUrl = function(mediaId){
+                return mediaUrl+mediaId;
+            };
+
+            self.postText = dateUtils.addTags;
+            self.parseDate = dateUtils.parseDate;
         }
-
-        updateMe(self.top50);
-
-        var loaded = false;
-        $scope.$on('LastElementReached', function(){
-            $scope.$evalAsync(function() {
-                setTimeout(function () {
-                    loadCards();
-                }, 10);
-
-                if(!loaded) {
-                    setTimeout(function () {
-                        loadCards();
-                    }, 50);
-                    setTimeout(function () {
-                        loadCards();
-                    }, 100);
-
-                    setTimeout(function () {
-                        loadCards();
-                    }, 200);
-                    setTimeout(function () {
-                        loadCards();
-                    }, 1000);
-                    setTimeout(function () {
-                        loadCards();
-                    }, 5000);
-
-                    loaded = true;
-                }
-            });
-        });
-
-        self.getMediaUrl = function(mediaId){
-            return mediaUrl+mediaId;
-        };
-
-        self.postText = dateUtils.addTags;
-        self.parseDate = dateUtils.parseDate;
-    }])
+    ])
 
     //lets us know when ngRepeat is over so we can
     //set cards
